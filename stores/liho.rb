6e4108423324671b157377c72ae11d4d7dcc24b7
Duncan Leo <duncanleo97@gmail.com>
2018-08-05 16:06:06 +0800

    add LiHo

---
+++ stores/liho.rb
@@ -0,0 +1,20 @@
+module Stores
+  def self.liho
+    @driver.navigate.to 'http://www.streetdirectory.com/businessfinder/company_branch/163304/5890/'
+
+    wait = Selenium::WebDriver::Wait.new(timeout: 10) # seconds
+    wait.until { @driver.find_element(css: '#company_branch_container tr[id]') }
+    items = @driver.find_elements(css: '#company_branch_container tr[id]')
+    
+    items.map { |item_elem|
+      BobaShop.new(
+        item_elem.find_element(css: '.company_branch_name').text,
+        item_elem.find_element(css: '.company_branch_address').text,
+        item_elem.find_elements(css: '.company_branch_phone').any? ?
+          item_elem.find_element(css: '.company_branch_phone').text.scan(/Tel.+?:\s?(.+)/).flatten.first
+          : nil,
+        nil
+      )
+    }
+  end
+end
