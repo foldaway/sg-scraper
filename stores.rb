b21608e6d27a0a105b94d414585524e0012c71c7
Duncan Leo <duncanleo97@gmail.com>
2018-08-08 21:10:54 +0800

    add support for GOOGLE_CHROME_SHIM

--- stores.rb
+++ stores.rb
@@ -3,7 +3,8 @@ Dir["./stores/*.rb"].each {|file| require file }
 require_relative './model/boba_shop'
 
 module Stores
-  options = Selenium::WebDriver::Chrome::Options.new
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+  options = Selenium::WebDriver::Chrome::Options.new(chrome_bin ? { binary: chrome_bin } : {})
   options.add_argument('--headless')
   @driver = Selenium::WebDriver.for :chrome, options: options
 end
