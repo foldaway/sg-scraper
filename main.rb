b9ed319c71c79291899b289b6026bc92de6dedf8
Duncan Leo <duncanleo97@gmail.com>
2018-08-08 19:01:46 +0800

    remove other prints

--- main.rb
+++ main.rb
@@ -6,13 +6,11 @@ onemap_client = OneMapClient.new
 
 methods = Stores.methods(false)
 methods.each do |m|
-  puts "Processing #{m}"
   data.push *Stores.public_send(m)
 end
 
 for shop in data do
   post_code = shop.address.scan(/(\d{6})/).flatten.first
-  puts "Finding location for #{shop.title}. Postal: #{post_code}"
   shop.location = onemap_client.search(post_code).first
   sleep(0.5)
 end
