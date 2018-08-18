5c8ec7cad0a53231904492dcb2cec496e7aee927
Duncan Leo <duncanleo97@gmail.com>
2018-08-18 11:15:58 +0800

    hotfix Sharetea

--- stores/sharetea.rb
+++ stores/sharetea.rb
@@ -5,24 +5,28 @@ module Stores
     wait = Selenium::WebDriver::Wait.new(timeout: 10) # seconds
     wait.until { @driver.find_element(css: '#nCS1') }
 
-    # Select Singapore
-    option = Selenium::WebDriver::Support::Select.new(@driver.find_element(css: '#nCS1'))
-    option.select_by(:text, 'Singapore')
+    country_select = @driver.find_element(css: '#nCS1')
+    city_select = @driver.find_element(css: '#nCS2')
+    country_dropdown = Selenium::WebDriver::Support::Select.new(country_select)
+    city_dropdown = Selenium::WebDriver::Support::Select.new(city_select)
 
-    locations_box = @driver.find_element(css: '#locations-box')
-    locations_box_html = locations_box.attribute('innerHTML')
+    # Set country and city
+    country_dropdown.select_by(:text, 'Singapore')
+    sleep 1
+    city_dropdown.select_by(:index, 0)
 
-    wait.until { locations_box.attribute('innerHTML') != locations_box_html }
+    sleep 1
     items = @driver.find_elements(css: '.locatBox')
     
     items.map { |item_elem|
+      address = item_elem.find_element(css: '.addr').text
       BobaShop.new(
         item_elem.find_element(css: 'h4').text,
-        item_elem.find_element(css: '.addr').text,
+        address.gsub(/Woodlabd/i, 'Woodlands'),
         nil,
         nil,
         'Sharetea'
       )
-      }.reject { |shop| shop.title.empty? }
+    }.reject { |shop| shop.title.empty? }
   end
 end
