a5cc698d7be3505759ba6589bda720e3768be186
Duncan Leo <duncanleo97@gmail.com>
2018-08-18 14:44:05 +0800

    create JSON + upload from main.rb directly

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require_relative 'stores'
 require_relative './util/onemap'
 
@@ -6,7 +7,10 @@ onemap_client = OneMapClient.new
 
 methods = Stores.methods(false)
 methods.each do |m|
-  data.push *Stores.public_send(m)
+  puts "[#{m}] Scraping started"
+  shops = Stores.public_send(m)
+  data.push *shops
+  puts "[#{m}] Scraped #{shops.size} shops"
 end
 
 for shop in data do
@@ -18,7 +22,14 @@ for shop in data do
   unless location_results.nil?
     shop.location = location_results.first
   end
+  puts "[OneMap] '#{location_search_term}' => #{JSON.generate(shop.location)}"
   sleep(0.5)
 end
 
-puts JSON.pretty_generate(data)
+Dir.mkdir('temp') unless File.exists?('temp')
+File.delete('temp/data.json') if File.exists?('temp/data.json')
+File.write('temp/data.json', JSON.pretty_generate(data))
+
+%x(dpl --provider=pages --committer-from-gh --github-token=$GITHUB_TOKEN --repo=$GITHUB_REPO --local-dir=temp)
+
+FileUtils.remove_dir('temp')
