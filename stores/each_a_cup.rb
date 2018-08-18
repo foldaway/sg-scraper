2a98204081d69a15061599029879bfa7ad10841c
Ivan Tan <ivantjh97@gmail.com>
2018-08-18 19:15:29 +0800

    Format Each-A-Cup title

--- stores/each_a_cup.rb
+++ stores/each_a_cup.rb
@@ -1,5 +1,7 @@
 module Stores
   def self.each_a_cup
+    title_regex = Regexp.new('\(([\w\s]*)\)')
+
     ['Central', 'North', 'West', 'East'].map { |region|
       @driver.navigate.to "http://www.each-a-cup.com/home/outlets/#{region}"
 
@@ -7,9 +9,14 @@ module Stores
       wait.until { @driver.find_element(css: '.service-item') }
       items = @driver.find_elements(css: '.service-item')
       sleep(0.2)
+
       items.map { |item_elem|
+        raw_title = item_elem.find_element(css: 'h3').text
+        title_set = title_regex.match(raw_title)
+        title = title_set == nil ? raw_title : title_set[1]
+
         BobaShop.new(
-          item_elem.find_element(css: 'h3').text,
+          title,
           item_elem.find_elements(css: 'p').first.text,
           item_elem.find_elements(css: 'p')[1].text,
           nil,
