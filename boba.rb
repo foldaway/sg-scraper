36d968ca92400344a7e7bb69e5cdc6da45548190
Duncan Leo <duncanleo97@gmail.com>
2018-10-21 12:56:52 +0800

    use headless mode only in Heroku

--- boba.rb
+++ boba.rb
@@ -5,6 +5,8 @@ require_relative './model/boba_shop'
 module Boba
   chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
   options = Selenium::WebDriver::Chrome::Options.new(chrome_bin ? { binary: chrome_bin } : {})
-  options.add_argument('--headless')
+  if ENV.has_key?('GITHUB_TOKEN')
+    options.add_argument('--headless')
+  end
   @driver = Selenium::WebDriver.for :chrome, options: options
 end
