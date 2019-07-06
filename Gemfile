8c92468e48ca41c9e076cf0628e8b9687d98f7f2
Duncan Leo <hello@duncanleo.me>
2019-07-06 22:46:55 +0800

    more boilerplate

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
+gemspec
+
 gem "selenium-webdriver", "~> 3.14"
 
 gem "pry", "~> 0.11.3", :group => [:development]
---
+++ spec/sg/scraper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Sg::Scraper do
+  it "has a version number" do
+    expect(Sg::Scraper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
