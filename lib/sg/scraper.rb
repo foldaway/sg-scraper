3e3e599d3a0b9b33510dfd4f14f8f75b8f18134e
Duncan Leo <hello@duncanleo.me>
2019-09-29 16:34:51 +0800

    skip over individual methods upon errors

--- lib/sg/scraper.rb
+++ lib/sg/scraper.rb
@@ -30,25 +30,25 @@ module Sg
         [method.to_sym]
       end
       
-      begin
-        methods.each do |m|
+      methods.each do |m|
+        begin
           puts "[#{m}] Scraping started"
           data = instance.send(m)
           results.push *data
           puts "[#{m}] Scraped #{data.size} data"
+        rescue => e
+          puts e
+          puts e.backtrace
+  
+          base64_data = instance.instance_variable_get(:@driver).screenshot_as(:base64)
+  
+          response = RestClient.post("https://api.imgbb.com/1/upload?key=b0bffdbc05689196b03fc1d3ae9ad5b4", {
+            :image => base64_data,
+            :name => "sg-scraper on #{RUBY_PLATFORM}"
+          })
+          img_url = JSON.parse(response)['data']['url_viewer']
+          puts "An error occurred, driver screenshot captured at #{img_url}"
         end
-      rescue => e
-        puts e
-        puts e.backtrace
-
-        base64_data = instance.instance_variable_get(:@driver).screenshot_as(:base64)
-
-        response = RestClient.post("https://api.imgbb.com/1/upload?key=b0bffdbc05689196b03fc1d3ae9ad5b4", {
-          :image => base64_data,
-          :name => "sg-scraper on #{RUBY_PLATFORM}"
-        })
-        img_url = JSON.parse(response)['data']['url_viewer']
-        puts "An error occurred, driver screenshot captured at #{img_url}"
       end
       results
     end
