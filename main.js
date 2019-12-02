a8d8724178f6d748a8a509edc5f53664bc22fd35
Duncan Leo <hello@duncanleo.me>
2019-12-02 20:52:05 +0800

    chore: initial Node.js setup

---
+++ main.js
@@ -0,0 +1,8 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://example.com');
+})();
