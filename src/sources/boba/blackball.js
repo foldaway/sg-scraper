0c72506d444911293a88b070a4259b77384d5222
Duncan Leo <hello@duncanleo.me>
2019-12-02 23:07:45 +0800

    feat: port BlackBall to JS

---
+++ src/sources/boba/blackball.js
@@ -0,0 +1,20 @@
+import './model.js';
+
+/**
+ * @param {import('puppeteer').Browser} browser
+ * @returns {Promise<Boba>}
+ */
+export default async function blackball(browser) {
+  const page = await browser.newPage();
+  await page.goto('http://blackball.com.sg/index.php/outlet-location/');
+  return page.evaluate(() => {
+    const items = [...document.querySelectorAll('.location')];
+
+    return items.map((item) => ({
+      title: item.querySelector('.location-title-pro').textContent.trim(),
+      address: item.querySelector('.location-address-pro').textContent.trim(),
+      openingHours: item.querySelector('.location-time-pro').textContent.trim(),
+      chain: 'BlackBall',
+    }));
+  });
+}
--- src/sources/boba/index.js
+++ src/sources/boba/index.js
@@ -1,3 +1,4 @@
 export function toString() { return 'boba'; }
 export { default as koi } from './koi.js';
 export { default as gongCha } from './gong-cha.js';
+export { default as blackball } from './blackball.js';
