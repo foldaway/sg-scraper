47508dc451dc6f3e8fe7faaf6fedce1dda11d340
Duncan Leo <hello@duncanleo.me>
2019-12-02 23:05:36 +0800

    feat: port Gong Cha to JS

--- src/sources/boba/gong-cha.js
+++ src/sources/boba/gong-cha.js
@@ -1,3 +1,21 @@
-export default async function gongCha() {
-  return [];
+import './model.js';
+
+/**
+ * @param {import('puppeteer').Browser} browser
+ * @returns {Promise<Boba>}
+ */
+export default async function gongCha(browser) {
+  const page = await browser.newPage();
+  await page.goto('http://www.gong-cha-sg.com/stores/');
+  await page.waitForSelector('.item', { timeout: 5000 });
+  return page.evaluate(() => {
+    const items = [...document.querySelectorAll('.item')];
+
+    return items.map((item) => ({
+      title: item.querySelector('.p-title').textContent,
+      address: item.querySelector('.p-area').textContent,
+      openingHours: item.querySelector('.p-time').textContent,
+      chain: 'Gong Cha',
+    }));
+  });
 }
