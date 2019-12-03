9927d9901ec5c1049d4cfc56230495fbe06dffb1
Duncan Leo <hello@duncanleo.me>
2019-12-03 22:04:09 +0800

    fix: disable Chromium sandbox in production

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ if (!fs.existsSync('temp')) {
 const main = async () => {
   const browser = await puppeteer.launch({
     headless: isProduction,
+    args: isProduction ? ['--no-sandbox'] : [],
   });
 
   for (const module of MODULES) {
