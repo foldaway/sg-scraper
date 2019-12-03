d6c8c9f6159282b9b97fa7a506b7ef6b33413428
Duncan Leo <hello@duncanleo.me>
2019-12-03 21:12:16 +0800

    feat: port DBS to JS

---
+++ src/sources/atm/dbs.js
@@ -0,0 +1,44 @@
+import './model.js';
+
+/**
+ * @param {import('puppeteer').Browser} browser
+ * @returns {Promise<Boba[]>}
+ */
+export default async function dbs(browser) {
+  const page = await browser.newPage();
+  await page.goto('https://www.dbs.com.sg/index/locator.page');
+  await page.waitForSelector('.jspContainer', { timeout: 5000 });
+  await page.waitForSelector('#selectBranch', { timeout: 5000 });
+
+  // Filter to ATM
+  await page.evaluate(() => {
+    document.querySelector('#selectBranch').click();
+
+    document.querySelector('div[name="DBS"] .service-name').click();
+    document.querySelector('div[name="DL"] .service-name').click();
+    document.querySelector('div[name="ATM"] .service-name').click();
+    document.querySelector('#listClose').click();
+  });
+
+  await page.waitForSelector('.address', { timeout: 5000 });
+
+  return page.evaluate(async () => {
+    const pageNumbers = [...document.querySelectorAll('.navnum')];
+    return pageNumbers.map((pageNumElement) => {
+      pageNumElement.click();
+      return [...document.querySelectorAll('div.store')].map((item) => {
+        item.scrollIntoView();
+
+        const address = item.querySelector('.address');
+        const postalCode = item.querySelector('.postal_code');
+
+        return {
+          location: item.querySelector('.title').textContent.trim(),
+          address: `${address ? address.textContent.trim() : ''}\n${postalCode ? item.querySelector('.postal_code').textContent.trim() : ''}`,
+          openingHours: '24/7',
+          bank: 'DBS',
+        };
+      });
+    }).reduce((a, b) => a.concat(b)); // flatten 2D array
+  });
+}
--- src/sources/atm/index.js
+++ src/sources/atm/index.js
@@ -1,2 +1,3 @@
 export { default as ocbc } from './ocbc.js';
+export { default as dbs } from './dbs.js';
 export function toString() { return 'atm'; }
