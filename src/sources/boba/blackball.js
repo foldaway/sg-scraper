86cd5962bac4508f2d5b15da9eeda835eb5455b6
Duncan Leo <hello@duncanleo.me>
2019-12-06 23:49:32 +0800

    feat: convert blackball to auto-parse

--- src/sources/boba/blackball.js
+++ src/sources/boba/blackball.js
@@ -1,24 +1,39 @@
 import Promise from 'bluebird';
 import './model.js';
 import autoLocation from '../../util/auto-location.js';
+import autoParse from '../../util/auto-parse.js';
 
 /**
  * @param {import('puppeteer').Browser} browser
  * @returns {Promise<Boba[]>}
  */
 export default async function blackball(browser) {
-  const page = await browser.newPage();
-  await page.goto('http://blackball.com.sg/index.php/outlet-location/');
-  const outlets = await page.evaluate(() => {
-    const items = [...document.querySelectorAll('.location')];
-
-    return items.map(item => ({
-      title: item.querySelector('.location-title-pro').textContent.trim(),
-      address: item.querySelector('.location-address-pro').textContent.trim(),
-      openingHours: item.querySelector('.location-time-pro').textContent.trim(),
-      chain: 'BlackBall',
-    }));
-  });
-  await page.close();
-  return Promise.map(outlets, autoLocation, {concurrency: 1});
+  const {outlets} = await autoParse(browser, [
+    {
+      type: 'navigate',
+      url: 'http://blackball.com.sg/index.php/outlet-location/',
+    },
+    {
+      id: 'locations',
+      type: 'elementsQuery',
+      selector: '.location',
+    },
+    {
+      id: 'outlets',
+      type: 'iterator',
+      collectionId: 'locations',
+      childSteps: [
+        {
+          id: 'outlet',
+          type: 'evaluatePage',
+          func: location => ({
+            title: location.querySelector('.location-title-pro').textContent.trim(),
+            address: location.querySelector('.location-address-pro').textContent.trim(),
+            openingHours: location.querySelector('.location-time-pro').textContent.trim(),
+          }),
+        },
+      ],
+    },
+  ]);
+  return Promise.map(outlets, ({outlet}) => autoLocation(outlet), {concurrency: 1});
 }
