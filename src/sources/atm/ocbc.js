0c2102a3b2ebc24ac928da7de5ca6a8eb7d11188
Duncan Leo <hello@duncanleo.me>
2019-12-07 00:32:54 +0800

    feat: convert ocbc to auto-parse

--- src/sources/atm/ocbc.js
+++ src/sources/atm/ocbc.js
@@ -1,32 +1,61 @@
 import Promise from 'bluebird';
 import './model.js';
 import autoLocation from '../../util/auto-location.js';
+import autoParse from '../../util/auto-parse.js';
 
 /**
  * @param {import('puppeteer').Browser} browser
  * @returns {Promise<Boba[]>}
  */
 export default async function ocbc(browser) {
-  const page = await browser.newPage();
-  await page.goto('https://www.ocbc.com/personal-banking/locate-us.html');
-  await page.waitForSelector('#tab2', {timeout: 5000});
-  const atms = await page.evaluate(() => {
-    const listViewButton = document.querySelector('#tab2');
-    listViewButton.scrollIntoView();
-    listViewButton.click();
-    const items = [...document.querySelectorAll('.address-column')];
+  const {atms} = await autoParse(browser, [
+    {
+      type: 'navigate',
+      url: 'https://www.ocbc.com/personal-banking/locate-us.html',
+    },
+    {
+      type: 'elementWait',
+      selector: '#tab2',
+    },
+    {
+      type: 'elementScrollIntoView',
+      selector: '#tab2',
+    },
+    {
+      type: 'elementClick',
+      selector: '#tab2',
+    },
+    {
+      id: 'addressColumns',
+      type: 'elementsQuery',
+      selector: '.address-column',
+    },
+    {
+      id: 'atms',
+      type: 'iterator',
+      collectionId: 'addressColumns',
+      childSteps: [
+        {
+          id: 'atm',
+          type: 'elementQueryShape',
+          queryShape: {
+            title: 'font',
+            address: ['font', location => location.match(/(\d{6})/)[0]],
+          },
+        },
+        {
+          type: 'mutateState',
+          func: state =>
+            Object.assign(state, {
+              atm: Object.assign(state.atm, {
+                openingHours: '24/7',
+                bank: 'OCBC',
+              }),
+            }),
+        },
+      ],
+    },
+  ]);
 
-    return items.map(item => {
-      const location = item.querySelector('font').textContent.trim();
-      return {
-        title: location,
-        address: location.match(/(\d{6})/)[0],
-        openingHours: '24/7',
-        bank: 'OCBC',
-      };
-    });
-  });
-
-  await page.close();
-  return Promise.map(atms, autoLocation, {concurrency: 1});
+  return Promise.map(atms, ({atm}) => autoLocation(atm), {concurrency: 1});
 }
