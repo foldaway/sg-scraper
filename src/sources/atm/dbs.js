aaad006482d4d6ffad3b5bc1b8c84029d88b3f50
Duncan Leo <hello@duncanleo.me>
2019-12-06 23:49:43 +0800

    feat: convert dbs to auto-parse

--- src/sources/atm/dbs.js
+++ src/sources/atm/dbs.js
@@ -1,53 +1,112 @@
 import Promise from 'bluebird';
 import './model.js';
 import autoLocation from '../../util/auto-location.js';
+import autoParse from '../../util/auto-parse.js';
 
 /**
  * @param {import('puppeteer').Browser} browser
  * @returns {Promise<Boba[]>}
  */
 export default async function dbs(browser) {
-  const page = await browser.newPage();
-  await page.goto('https://www.dbs.com.sg/index/locator.page');
-  await page.waitForSelector('.jspContainer', {timeout: 5000});
-  await page.waitForSelector('#selectBranch', {timeout: 5000});
+  const {atms} = await autoParse(browser, [
+    {
+      type: 'navigate',
+      url: 'https://www.dbs.com.sg/index/locator.page',
+    },
+    {
+      type: 'elementWait',
+      selector: '.jspContainer',
+    },
+    {
+      type: 'elementWait',
+      selector: '#selectBranch',
+    },
+    {
+      type: 'elementClick',
+      selector: '#selectBranch',
+    },
+    {
+      type: 'elementWait',
+      selector: 'div.list-item[name="DBS"] .service-name',
+    },
+    {
+      type: 'elementClick',
+      selector: 'div.list-item[name="DBS"] .service-name',
+    },
+    {
+      type: 'elementClick',
+      selector: 'div.list-item[name="DL"] .service-name',
+    },
+    {
+      type: 'elementClick',
+      selector: 'div.list-item[name="ATM"] .service-name',
+    },
+    {
+      type: 'elementClick',
+      selector: '#listClose',
+    },
+    {
+      type: 'elementWait',
+      selector: '.address',
+    },
+    {
+      id: 'pageNumbers',
+      type: 'elementsQuery',
+      selector: '.navnum',
+    },
+    {
+      id: 'atms',
+      type: 'iterator',
+      collectionId: 'pageNumbers',
+      childSteps: [
+        {
+          type: 'elementClick',
+        },
+        {
+          type: 'elementWait',
+          selector: 'div.store',
+        },
+        {
+          id: 'stores',
+          type: 'elementsQuery',
+          selector: 'div.store',
+        },
+        {
+          id: 'atmsChunk',
+          type: 'iterator',
+          collectionId: 'stores',
+          childSteps: [
+            {
+              id: 'atm',
+              type: 'evaluatePage',
+              func: iteratee => {
+                iteratee.scrollIntoView();
 
-  // Filter to ATM
-  await page.evaluate(() => {
-    document.querySelector('#selectBranch').click();
+                const address = iteratee.querySelector('.address');
+                const postalCode = iteratee.querySelector('.postal_code');
 
-    document.querySelector('div[name="DBS"] .service-name').click();
-    document.querySelector('div[name="DL"] .service-name').click();
-    document.querySelector('div[name="ATM"] .service-name').click();
-    document.querySelector('#listClose').click();
-  });
+                return {
+                  title: iteratee.querySelector('.title').textContent.trim(),
+                  address: `${address ? address.textContent.trim() : ''}\n${
+                    postalCode ? iteratee.querySelector('.postal_code').textContent.trim() : ''
+                  }`,
+                  openingHours: '24/7',
+                  bank: 'DBS',
+                };
+              },
+            },
+          ],
+        }
+      ],
+    },
+    {
+      type: 'mutateState',
+      func: state =>
+        Object.assign(state, {
+          atms: state.atms.map(atm => atm.atmsChunk).reduce((a, b) => a.concat(b)),
+        }),
+    },
+  ]);
 
-  await page.waitForSelector('.address', {timeout: 5000});
-
-  const atms = await page.evaluate(async () => {
-    const pageNumbers = [...document.querySelectorAll('.navnum')];
-    return pageNumbers
-      .map(pageNumElement => {
-        pageNumElement.click();
-        return [...document.querySelectorAll('div.store')].map(item => {
-          item.scrollIntoView();
-
-          const address = item.querySelector('.address');
-          const postalCode = item.querySelector('.postal_code');
-
-          return {
-            title: item.querySelector('.title').textContent.trim(),
-            address: `${address ? address.textContent.trim() : ''}\n${
-              postalCode ? item.querySelector('.postal_code').textContent.trim() : ''
-            }`,
-            openingHours: '24/7',
-            bank: 'DBS',
-          };
-        });
-      })
-      .reduce((a, b) => a.concat(b)); // flatten 2D array
-  });
-
-  await page.close();
-  return Promise.map(atms, autoLocation, {concurrency: 1});
+  return Promise.map(atms, ({atm}) => autoLocation(atm), {concurrency: 1});
 }
