0e0432c65113fff82c31151c8119467a6baedfbf
Duncan Leo <hello@duncanleo.me>
2019-12-07 01:13:03 +0800

    feat: convert koi to auto-parse

--- src/sources/boba/koi.js
+++ src/sources/boba/koi.js
@@ -1,26 +1,50 @@
 import Promise from 'bluebird';
 import './model.js';
 import autoLocation from '../../util/auto-location.js';
+import autoParse from '../../util/auto-parse.js';
 
 /**
  * @param {import('puppeteer').Browser} browser
  * @returns {Promise<Boba[]>}
  */
 export default async function koi(browser) {
-  const page = await browser.newPage();
-  await page.goto('https://www.koithe.com/en/global/koi-singapore');
-  const outlets = await page.evaluate(() => {
-    const items = [...document.querySelectorAll('.global-wrap .item')];
+  const {outlets} = await autoParse(browser, [
+    {
+      type: 'navigate',
+      url: 'https://www.koithe.com/en/global/koi-singapore',
+    },
+    {
+      id: 'items',
+      type: 'elementsQuery',
+      selector: '.global-wrap .item',
+    },
+    {
+      id: 'outlets',
+      type: 'iterator',
+      collectionId: 'items',
+      childSteps: [
+        {
+          id: 'outlet',
+          type: 'elementQueryShape',
+          queryShape: {
+            title: '.titlebox',
+            address: '.txt a',
+            phone: '.titlebox',
+            openingHours: '.txt',
+          },
+        },
+        {
+          type: 'mutateState',
+          mutateFunc: state =>
+            Object.assign(state, {
+              outlet: Object.assign(state.outlet, {
+                chain: 'KOI',
+              }),
+            }),
+        },
+      ],
+    },
+  ]);
 
-    return items.map(item => ({
-      title: item.querySelector('.titlebox').textContent.trim(),
-      address: item.querySelector('.txt a').textContent.trim(),
-      phone: item.querySelector('.titlebox').textContent.trim(),
-      openingHours: item.querySelector('.txt').textContent.trim(),
-      chain: 'Koi',
-    }));
-  });
-
-  await page.close();
-  return Promise.map(outlets, autoLocation, {concurrency: 1});
+  return Promise.map(outlets, ({outlet}) => autoLocation(outlet), {concurrency: 1});
 }
