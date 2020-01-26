5d4e9cee48ee5c01b8794980f706d13cde7f8d94
Daniel Lee <minqiang8194@gmail.com>
2020-01-26 13:37:18 +0800

    testing of main.js with game module

--- modules.js
+++ modules.js
@@ -1,5 +1,6 @@
 
 import * as atm from './src/sources/atm/index.js';
 import * as boba from './src/sources/boba/index.js';
+import * as game from './src/sources/game/index.js';
 
-export default [atm, boba];
+export default [game];
