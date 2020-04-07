2d8026dd7dbc5d89fb5ed06be88bcee6f8e03419
Daniel Lee <explosivecow@me.com>
2020-04-07 09:44:43 +0800

    feat: add scraping for animal crossing fishes (PR: #19)
    * add scraping for animal crossing fishes
    
    * add type = fish
    
    * change filtering method and add bugs

---
+++ src/sources/acnh/bug.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-shadow */
+import './model.js';
+import autoParse from '../../util/auto-parse.js';
+
+/**
+ * @param {import('puppeteer').Browser} browser
+ * @returns {<any[]>}
+ */
+export default async function bug(browser) {
+  const bugs = await autoParse(browser, [
+    {
+      type: 'navigate',
+      url:
+        'https://www.polygon.com/animal-crossing-new-horizons-switch-acnh-guide/2020/3/24/21191276/insect-bug-locations-times-month-day-list-critterpedia',
+    },
+    {
+      type: 'elementsQuery',
+      selector: 'tr',
+    },
+    {
+      type: 'iterator',
+      childSteps: [
+        {
+          type: 'elementQueryShape',
+          queryShape: {
+            name: 'td:nth-child(2)',
+            location: 'td:nth-child(3)',
+            sellPrice: 'td:nth-child(4)',
+            season: 'td:nth-child(6)',
+            time: 'td:nth-child(5)',
+          },
+        },
+      ],
+    },
+  ]);
+
+  const data = bugs.filter(bug => bug.name !== null).map(bug => Object.assign(bug, {type: 'Bug'}));
+  console.log(data);
+  return data;
+}
---
+++ src/sources/acnh/model.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef Item
+ * @property {string} type
+ * @property {string} name
+ * @property {string} sellPrice
+ * @property {string} season
+ * @property {string} location
+ * @property {string} time
+ */
