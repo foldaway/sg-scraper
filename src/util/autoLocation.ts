72d67813d12567d99f6115975c0b816ae1d88659
Duncan Leo <hello@duncanleo.me>
2020-08-09 18:26:37 +0800

    refactor: autoLocation and postalCode to TS

--- src/util/autoLocation.ts
+++ src/util/autoLocation.ts
@@ -1,13 +1,16 @@
 import lookupLocation from '../location/lookup.js';
-import postalCode from './postal-code.js';
+import postalCode from './postalCode.js';
 
 /**
  * Helper function to automatically set a place's address
  * to its OneMap lookup address
- * @param {object} obj
  */
-export default async function autoLocation(obj) {
-  const addressLookup = await lookupLocation(postalCode(obj.address) || obj.address);
+export default async function autoLocation<T>(
+  obj: T & { address: string }
+): Promise<T> {
+  const addressLookup = await lookupLocation(
+    postalCode(obj.address) || obj.address
+  );
   return Object.assign(obj, {
     location: addressLookup ? addressLookup.results[0] : null,
   });
