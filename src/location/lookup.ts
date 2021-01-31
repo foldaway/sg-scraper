d3ab576b3c7a063ceefbdaf19b41662d8ebf1f42
Duncan Leo <hello@duncanleo.me>
2021-01-31 17:03:51 +0800

    chore: allow dev without Redis

--- src/location/lookup.ts
+++ src/location/lookup.ts
@@ -1,6 +1,13 @@
 import Redis from 'ioredis';
 import search, { Response } from '../onemap/onemap';
 
+let client = new Redis(process.env.REDIS_URL);
+
+client.on('error', () => {
+  client = null;
+  console.warn('[DEV] Error connecting to Redis, proceeding without it');
+});
+
 /**
  * Lookup a location (with Redis caching)
  * @param {string} rawText
@@ -8,12 +15,19 @@ import search, { Response } from '../onemap/onemap';
 export default async function lookupLocation(
   rawText: string
 ): Promise<Response> {
-  const client = new Redis(process.env.REDIS_URL);
+  if (client != null) {
+    const isCachedResultAvailable = await client.exists(rawText);
 
-  const results = (await client.exists(rawText))
-    ? JSON.parse(await client.get(rawText))
-    : await search(rawText);
-  await client.set(rawText, JSON.stringify(results));
-  await client.quit();
+    if (isCachedResultAvailable) {
+      return JSON.parse(await client.get(rawText));
+    }
+  }
+
+  const results = await search(rawText);
+
+  if (client != null) {
+    await client.set(rawText, JSON.stringify(results));
+    await client.quit();
+  }
   return results;
 }
