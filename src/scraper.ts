8e9734b316b30c69b5054e24889cfcbb4281b153
Duncan Leo <hello@duncanleo.me>
2021-01-31 18:37:22 +0800

    chore: add special clause for ARM macOS

--- src/scraper.ts
+++ src/scraper.ts
@@ -64,10 +64,15 @@ async function boba(browser: Browser) {
 }
 
 async function scraper() {
+  const isARMMac = process.arch === 'arm64' && process.platform === 'darwin';
+
   const browser = await puppeteer.launch({
     headless: isProduction,
     defaultViewport: null,
     args: isProduction ? ['--no-sandbox'] : [],
+    executablePath: isARMMac
+      ? '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      : undefined,
   });
 
   await atm(browser);
