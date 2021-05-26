d83edb6d1580aeb8497d23a596b07b269c30bda0
Amos <alphatrl1997@gmail.com>
2021-05-27 01:27:10 +0800

    cleanup
    * remove unnecessary code

--- src/sources/hawker/index.ts
+++ src/sources/hawker/index.ts
@@ -1,23 +1,12 @@
+import { orderBy } from 'lodash';
 import moment, { Moment } from 'moment-timezone';
 
 import { Hawker, HawkerRaw } from './model';
-import autoParse from '../../util/data-gov-api';
+import dataGovApi from '../../util/data-gov-api';
 
 const TODAY = moment();
 const KEYS = ['q1', 'q2', 'q3', 'q4', 'others'];
 const RESOURCE_ID = 'b80cb643-a732-480d-86b5-e03957bc82aa';
-
-const splitDate = (dateString: string) => {
-  const data = dateString.split('/');
-  if (data.length !== 3) {
-    return undefined;
-  }
-  return {
-    year: data[2],
-    month: data[1].length === 1 ? '0' + data[1] : data[1],
-    day: data[0].length === 1 ? '0' + data[0] : data[0],
-  };
-};
 
 /**
  * Get the upcoming close date and the reason behind the closure
@@ -28,52 +17,38 @@ const splitDate = (dateString: string) => {
 const getCloseDetails = (hawker: HawkerRaw) => {
   const upcomingClosures: { key: string; endDate: Moment }[] = [];
 
-  // Convert a date string into a Date object, factoring in SG timezone
-  const convertFromDateString = (dateString: string): Moment => {
-    const split = splitDate(dateString);
-    if (!split) {
-      return moment.tz(dateString, 'Asia/Singapore');
-    }
-    return moment.tz(
-      `${split.year}-${split.month}-${split.day} 00:00`,
-      'Asia/Singapore'
-    );
-  };
-
   for (const key of KEYS) {
     const tempKey =
       key === 'others' ? 'other_works_enddate' : `${key}_cleaningenddate`;
-    const rawEndDate = convertFromDateString(hawker[tempKey]);
+    const rawEndDate = moment.tz(`${hawker[tempKey]} 00:00`, 'Asia/Singapore');
     rawEndDate.add(1, 'days');
 
-    if (TODAY < rawEndDate) {
+    if (TODAY.isBefore(rawEndDate)) {
       upcomingClosures.push({ key, endDate: rawEndDate });
     }
   }
 
   if (upcomingClosures.length === 0) {
-    return {
-      closeStartDate: null,
-      closeEndDate: null,
-      closeReason: null,
-    };
+    return null;
   }
 
-  const upcoming = upcomingClosures.sort((a, b) =>
-    a.endDate.diff(b.endDate)
-  )[0];
+  const upcoming = orderBy(upcomingClosures, ['endDate'], ['asc'])[0];
 
-  const closeStartDate =
+  const startKey =
     upcoming.key === 'others'
-      ? convertFromDateString(hawker.other_works_startdate)
-      : convertFromDateString(hawker[`${upcoming.key}_cleaningstartdate`]);
-  const closeReason =
-    upcoming.key === 'others' ? hawker.remarks_other_works : 'cleaning';
+      ? 'other_works_startdate'
+      : `${upcoming.key}_cleaningstartdate`;
+
+  const closeStartDate = moment.tz(
+    `${hawker[startKey]} 00:00`,
+    'Asia/Singapore'
+  );
 
   return {
     closeStartDate: closeStartDate.valueOf(),
     closeEndDate: upcoming.endDate.valueOf(),
-    closeReason,
+    closeReason:
+      upcoming.key === 'others' ? hawker.remarks_other_works : 'cleaning',
   };
 };
 
@@ -81,8 +56,8 @@ export default async function hawker(): Promise<Hawker[]> {
   const params = {
     limit: 150,
   };
-  const response = await autoParse(RESOURCE_ID, params);
-  const hawkersList = (<unknown>response?.result.records) as HawkerRaw[];
+  const response = await dataGovApi<HawkerRaw>(RESOURCE_ID, params);
+  const hawkersList = response?.result.records;
 
   const result = hawkersList.map((hawkerObj) => {
     const closureDetails = getCloseDetails(hawkerObj);
